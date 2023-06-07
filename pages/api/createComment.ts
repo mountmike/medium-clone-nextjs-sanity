649edac69f37804b57ff23a2a2f69a5d0064a995
Michael <tharratt.michael@gmail.com>
2023-06-07 14:59:12 +1000

    building out post comments in front end

---
+++ pages/api/createComment.ts
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default function createComment(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ name: 'John Doe' })
+}
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,15 +2,44 @@ import Header from '@/components/Header'
 import { client } from '../../sanity/lib/client'
 import { Post } from '@/typings'
 import { GetStaticProps } from 'next'
-import { urlForImage } from '@/sanity/lib/image';
+import { urlForImage } from '@/sanity/lib/image'
 import PortableText from "react-portable-text"
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { log } from 'console'
 
 interface Props {
     post: Post;
 }
 
+interface IFormInput {
+    _id: string;
+    name: string;
+    email: string;
+    comment: string;
+}
+
 function Post({ post }: Props) {
-  return (
+
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors },
+    } = useForm<IFormInput>()
+
+    const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+        await fetch('/api/createComment', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        }).then(() => {
+            console.log(data);
+            
+        }).catch((err) => {
+            console.log(err);
+            
+        })
+    }
+
+    return (
     <main>
         <Header />
         <img 
@@ -35,14 +64,92 @@ function Post({ post }: Props) {
 
             <div>
                 <PortableText 
-                    dataset={process.env}
-                    projectId={}
+                    dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+                    projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
                     content={post.body}
-                    serializers={}
+                    serializers={{
+                        h1: (props: any) => (
+                            <h1 className="text-2xl font-bold my-5" {... props} />
+                        ),
+                        h2: (props: any) => (
+                            <h2 className="text-xl font-bold my-5" {... props} />
+                        ),
+                        li: ({ children }: any) => (
+                            <li className="ml-4 list-disc">{children}</li>
+                        ),
+                        link: ({ href, children }: any) => (
+                            <a href={href} className='text-blue-500 hover:underline'>
+                                { children }
+                            </a>
+                        ),
+                    }}
                 />
             </div>
 
         </article>
+
+        <hr className='max-w-lg my-5 mx-auto border-yellow-500'/>
+        
+        <form 
+            className='flex flex-col p-5 max-w-2xl mb-10 mx-auto'
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <h3 className='text-3xl my-5'>Leave a comment?</h3>
+
+            <input 
+                {...register("_id")}
+                type="hidden" 
+                name='_id'
+                value={post._id}
+            />
+
+            <label className='block mb-5'>
+                <span className='text-gray-700'>Name</span>
+                <input
+                    {...register("name", { required: true })}
+                    className='shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500' 
+                    type="text" 
+                    placeholder='Mike Snow' 
+                />
+            </label>
+            <label className='block mb-5'>
+                <span className='text-gray-700'>Email</span>
+                <input 
+                {...register("email", { required: true })}
+                className='shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500' 
+                type="text" 
+                placeholder='time@jestyco.com' 
+            />
+            </label>
+            <label className='block mb-5'>
+                <span className='text-gray-700'>Comment</span>
+                <textarea
+                    {...register("comment", { required: true })} 
+                    className='shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500' 
+                    placeholder='Mike Snow' 
+                    rows={8} />
+            </label>
+
+            {/* errors will return when field validation fails*/}
+
+            <div className='flex flex-col p-5'>
+                {errors.name && (
+                    <span className='text-red-500'>The name field is required</span>
+                )}
+                 {errors.comment && (
+                    <span className='text-red-500'>The comment field is required</span>
+                )}
+                 {errors.email && (
+                    <span className='text-red-500'>The email field is required</span>
+                )}
+            </div>
+
+            <input 
+                type="submit" 
+                className='shaddow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline text-black font-bold py-2 px-3 rounded cursor-pointer' 
+            />
+
+        </form>
 
        
     </main>
